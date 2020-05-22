9365c1df949278d51784b34460bad344f366bb3e
Raju Kalyadapu <kalyadapuraju@hotmail.com>
2020-05-22 22:15:48 +0530

    ..

--- rj.j2ee.jdbc/src/main/java/com/rj/j2ee/jdbc/MetadataTest.java
+++ rj.j2ee.jdbc/src/main/java/com/rj/j2ee/jdbc/MetadataTest.java
@@ -1,9 +1,7 @@
 package com.rj.j2ee.jdbc;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
