7e2cd98410641a32f3b30fdc91a95f62cab98207
Raju Kalyadapu <kalyadapuraju@hotmail.com>
2020-05-22 22:11:29 +0530

    .

---
+++ rj.j2ee.jdbc/src/main/java/com/rj/j2ee/jdbc/MetadataTest.java
@@ -0,0 +1,38 @@
+package com.rj.j2ee.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MetadataTest {
+	@Autowired
+	private Connection con;
+	@Autowired
+	private Statement st;
+	@Autowired
+	private ResultSet rs;
+
+	public static void main(String[] args) throws Exception {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("application-context2.xml");
+		MetadataTest metaDataTst = ctx.getBean(MetadataTest.class);
+		metaDataTst.showStudentsData();
+		System.out.println(metaDataTst.con);
+		System.out.println(metaDataTst.st);
+		System.out.println(metaDataTst.rs);
+	}
+	public void showStudentsData() throws SQLException {
+		
+		
+		
+	}
+}
+
+	 
